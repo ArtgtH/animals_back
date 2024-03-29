41ea371d6f35b4885410873ae9e53d046d4be8f0
paha_koha <pashagerasimik@mail.ru>
2024-03-30 00:30:48 +0300

    Дополнение к последнему коммиту

--- src/main/java/com/animals_back/controllers/RESTController.java
+++ src/main/java/com/animals_back/controllers/RESTController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -29,11 +31,11 @@ public class RESTController {
     }
 
     @PutMapping("save-animal")
-    public ResponseEntity<String> saveAnimal(@Nullable @RequestParam("image") MultipartFile multipartFile,
+    public ResponseEntity<?> saveAnimal(@Nullable @RequestParam("image") MultipartFile multipartFile,
                                              @RequestParam("json") String json) throws IOException {
         try {
             animalService.saveAnimal(multipartFile, json);
-            return ResponseEntity.status(HttpStatus.CREATED).body("Зверёк был успешно сохранен.");
+            return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("message", "Зверек был успешно добавлен!"));
         } catch (AnimalAlreadyExistException exception) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
         }
