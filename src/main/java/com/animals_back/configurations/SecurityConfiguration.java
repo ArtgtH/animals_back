c287ab0a47628fe3bba01becb1042f4d369d96a1
paha_koha <pashagerasimik@mail.ru>
2024-07-20 18:34:19 +0300

    Добавил WebConfig для корректной обработки запросов и заголовков запросов. Добавил docker-compose

--- src/main/java/com/animals_back/configurations/SecurityConfiguration.java
+++ src/main/java/com/animals_back/configurations/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -62,11 +63,11 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
+//                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("api/info").authenticated()
-//                        .requestMatchers("api/delete-animal").hasRole("ADMIN")
-//                        .requestMatchers("api/save-animal").hasRole("ADMIN")
+                        .requestMatchers("api/delete-animal").hasRole("ADMIN")
+                        .requestMatchers("api/save-animal").hasRole("ADMIN")
                         .anyRequest().permitAll()
                 )
                 .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
